66103ee4d694031759aa0bec94362f4d291d4e75
brbenham1 <brbenham1@gmail.com>
2025-03-06 08:18:59 +1000

    Install nvim autopairs

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,4 +2,11 @@ return
 {
     -- Vim tmux navigator
     "christoomey/vim-tmux-navigator",
+
+    -- nvim-autopairs    
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+    },
 }

46c8454b2dce4884bb3fd7ec878700cfca2624a2
brbenham1 <brbenham1@gmail.com>
2026-04-03 07:37:04 +1000

    Move to treesitter main, update treesitter.nvim

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,82 +1,85 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		lazy = false,
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter").setup({
-				ensure_installed = {
-					"c",
-					"c_sharp",
-					"lua",
-					"vim",
-					"vimdoc",
-					"markdown",
-					"markdown_inline",
-					"query",
-					"latex",
-					"html",
-					"css",
-					"javascript",
-					"python",
-					"typescript",
-					"typst",
-				},
+			local ensure_installed = {
+				"c",
+				"c_sharp",
+				"lua",
+				"vim",
+				"vimdoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"latex",
+				"html",
+				"css",
+				"javascript",
+				"python",
+				"typescript",
+				"typst",
+			}
 
-				-- Automatically install missing parsers when entering buffer
-				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-				sync_install = false,
-				auto_install = true,
+			require("nvim-treesitter").install(ensure_installed)
 
-				highlight = {
-					enable = true,
-				},
+			require("nvim-treesitter").setup()
 
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<Leader>ss",
-						node_incremental = "<Leader>si",
-						scope_incremental = "<Leader>sc",
-						node_decremental = "<Leader>sd",
-					},
-				},
-
-				textobjects = {
-					select = {
-						enable = true,
-
-						lookahead = true,
-
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-
-							-- You can also use captures from other query groups like `locals.scm`
-							["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-						},
-
-						selection_modes = {
-							["@parameter.outer"] = "v", -- charwise
-							["@function.outer"] = "V", -- linewise
-							["@class.outer"] = "<c-v>", -- blockwise
-						},
-
-						-- If you set this to `true` (default is `false`) then any textobject is
-						-- extended to include preceding or succeeding whitespace. Succeeding
-						-- whitespace has priority in order to act similarly to eg the built-in
-						-- `ap`.
-						--
-						-- Can also be a function which gets passed a table with the keys
-						-- * query_string: eg '@function.inner'
-						-- * selection_mode: eg 'v'
-						-- and should return true or false
-						include_surrounding_whitespace = true,
-					},
-				},
-			})
+			-- -- Automatically install missing parsers when entering buffer
+			-- -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			-- sync_install = false,
+			-- auto_install = true,
+			--
+			-- highlight = {
+			--     enable = true,
+			-- },
+			--
+			-- incremental_selection = {
+			--     enable = true,
+			--     keymaps = {
+			--         init_selection = "<Leader>ss",
+			--         node_incremental = "<Leader>si",
+			--         scope_incremental = "<Leader>sc",
+			--         node_decremental = "<Leader>sd",
+			--     },
+			-- },
+			--
+			-- textobjects = {
+			--     select = {
+			--         enable = true,
+			--
+			--         lookahead = true,
+			--
+			--         keymaps = {
+			--             ["af"] = "@function.outer",
+			--             ["if"] = "@function.inner",
+			--             ["ac"] = "@class.outer",
+			--             ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+			--
+			--             -- You can also use captures from other query groups like `locals.scm`
+			--             ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+			--         },
+			--
+			--         selection_modes = {
+			--             ["@parameter.outer"] = "v", -- charwise
+			--             ["@function.outer"] = "V", -- linewise
+			--             ["@class.outer"] = "<c-v>", -- blockwise
+			--         },
+			--
+			--         -- If you set this to `true` (default is `false`) then any textobject is
+			--         -- extended to include preceding or succeeding whitespace. Succeeding
+			--         -- whitespace has priority in order to act similarly to eg the built-in
+			--         -- `ap`.
+			--         --
+			--         -- Can also be a function which gets passed a table with the keys
+			--         -- * query_string: eg '@function.inner'
+			--         -- * selection_mode: eg 'v'
+			--         -- and should return true or false
+			--         include_surrounding_whitespace = true,
+			--     },
+			-- },
 		end,
 	},
 	{
